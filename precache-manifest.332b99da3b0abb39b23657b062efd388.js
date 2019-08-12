f60ae271be4bc840e1d2b90e07bd3b56df62c58b
Mario Fraiß <m.fraiss@fraiss.at>
2019-08-13 00:40:10 +0200

    Added todo-app to the root

---
+++ precache-manifest.332b99da3b0abb39b23657b062efd388.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b73b9b6a722f82f66efb518af02af1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c37777d608eb33058f3b",
+    "url": "/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "acb9f0828f5aeec25e26",
+    "url": "/static/js/2.b84c114c.chunk.js"
+  },
+  {
+    "revision": "c37777d608eb33058f3b",
+    "url": "/static/js/main.a7026f13.chunk.js"
+  },
+  {
+    "revision": "d20c5c17c8b6f63ca18e",
+    "url": "/static/js/runtime~main.ebde4631.js"
+  }
+]);
