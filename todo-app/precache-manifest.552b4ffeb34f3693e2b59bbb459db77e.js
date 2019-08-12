08752949c77a92eff20030a4dd92186f28389f56
Mario Fraiß <m.fraiss@fraiss.at>
2019-08-13 00:15:42 +0200

    Added first version of todo-app

---
+++ todo-app/precache-manifest.552b4ffeb34f3693e2b59bbb459db77e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78ad649d8793928151ec20e900ed60a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be94bad583ea2dcd237c",
+    "url": "/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "68b359dd07fbce0a894c",
+    "url": "/static/js/2.b6a2bebc.chunk.js"
+  },
+  {
+    "revision": "be94bad583ea2dcd237c",
+    "url": "/static/js/main.b2c54cc1.chunk.js"
+  },
+  {
+    "revision": "d20c5c17c8b6f63ca18e",
+    "url": "/static/js/runtime~main.ebde4631.js"
+  }
+]);
